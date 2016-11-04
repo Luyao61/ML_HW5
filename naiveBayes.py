5e73eec41bd540c72f7b61050003960eb78e908d
Luyao Zhou <LuyaoMBP@Luyaos-MacBook-Pro.local>
2016-11-03 20:54:08 -0400

    day 1 8:53pm

--- naiveBayes.py
+++ naiveBayes.py
@@ -12,7 +12,17 @@ from sklearn.naive_bayes import MultinomialNB
 
 
 def transfer(fileDj, vocabulary):
-
+    wordnet_lemmatizer = WordNetLemmatizer()
+    raw = fileDj.read()
+    BOWDj = [0]*15
+    for token in raw.split( ):
+    #for token in nltk.word_tokenize(raw):
+        token_stemed = wordnet_lemmatizer.lemmatize(token)
+        if token_stemed in vocabulary:
+            index = vocabulary.index(token_stemed) + 1 # add 1 because index if saved for 'UNKNOWN'
+        else:
+            index = 0
+        BOWDj[index] = BOWDj[index] + 1
     return BOWDj
 
 
@@ -20,30 +30,38 @@ def loadData(Path):
     dict_predefined  = ['love', 'wonderful', 'best', 'great', 'superb',
                         'still', 'beautiful', 'bad', 'worst', 'stupid', 'waste',
                         'boring', '?', '!']
-    Xtrain = np.array([1400,15]):fill(0)
-    ytrain = np.array([1400,1]):fill(0)
-    Xtest = np.array([600,15]):fill(0)
-    ytest = np.array([600,1]):fill(0)
+    Xtrain = []
+    ytrain = []
+    Xtest = []
+    ytest = []
     train_path = Path + '/training_set'
     test_path = Path + '/test_set'
-    wordnet_lemmatizer = WordNetLemmatizer()
     #load train set neg files
-    i = 0
     for file in os.listdir( train_path+'/neg' ):
         f= open( train_path+'/neg' + '/' + file, 'rU')
-        raw = f.read()
-        for token in nltk.word_tokenize(raw):
-            token_stemed = wordnet_lemmatizer.lemmatize(token)
-            if token_stemed in dict_predefined:
-                index = dict_predefined.index(token_stemed) + 1 # add 1 because index if saved for 'UNKNOWN'
-            else:
-                index = 0
-            Xtrain[i][index] = Xtrain[i][index] + 1
-            ytrain[i] = 0
-        i = i +1
+        fearture_vector = transfer(f, dict_predefined)
+        Xtrain.append(fearture_vector)
+        ytrain.append([0])
+    #load train set pos files
+    for file in os.listdir( train_path+'/pos' ):
+        f= open( train_path+'/pos' + '/' + file, 'rU')
+        fearture_vector = transfer(f, dict_predefined)
+        Xtrain.append(fearture_vector)
+        ytrain.append([1])
+    print(len(Xtrain))
 
-
-
+    #load test set neg files
+    for file in os.listdir( test_path+'/neg' ):
+        f= open( test_path+'/neg' + '/' + file, 'rU')
+        fearture_vector = transfer(f, dict_predefined)
+        Xtest.append(fearture_vector)
+        ytest.append([0])
+    #load test set pos files
+    for file in os.listdir( test_path+'/pos' ):
+        f= open( test_path+'/pos' + '/' + file, 'rU')
+        fearture_vector = transfer(f, dict_predefined)
+        Xtest.append(fearture_vector)
+        ytest.append([0])
 
 
     return Xtrain, Xtest, ytrain, ytest
